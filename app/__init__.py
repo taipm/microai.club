33109bbe8962bad5efdbf4e19f06131c6e2e19b2
Phan Minh Tài <taipm.vn@gmail.com>
2023-03-10 23:39:00 +0700

    Refactor struct

--- app/__init__.py
+++ app/__init__.py
@@ -1,17 +1,35 @@
+# from flask import Flask
+
+# def create_app():
+#     # Tạo instance của Flask với tên app
+#     app = Flask(__name__)
+
+#     # Import các routes
+#     from app.routes.questions import question_bp
+#     #from app.routes.answers import answer_bp
+#     from app.routes.home import home_bp
+
+#     # Register các blueprint
+#     app.register_blueprint(question_bp)
+#     # app.register_blueprint(answer_bp)
+#     app.register_blueprint(home_bp)
+
+#     return app
+
 from flask import Flask
 
 def create_app():
     # Tạo instance của Flask với tên app
     app = Flask(__name__)
 
-    # Import các routes
-    from app.routes.questions import question_bp
-    #from app.routes.answers import answer_bp
-    from app.routes.home import home_bp
-
     # Register các blueprint
+    from .routes.questions import question_bp
     app.register_blueprint(question_bp)
-    # app.register_blueprint(answer_bp)
+
+    from .routes.answers import answers_bp
+    app.register_blueprint(answers_bp)
+
+    from .routes.home import home_bp
     app.register_blueprint(home_bp)
 
     return app
