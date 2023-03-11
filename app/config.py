eab5dbcd5b97fb5bfa1975493ea1eeb58447c144
Phan Minh Tài <taipm.vn@gmail.com>
2023-03-11 23:47:40 +0700

    Update

---
+++ app/config.py
@@ -0,0 +1,20 @@
+from configparser import ConfigParser
+
+# class Config:
+#     def __init__(self):
+#         self.config = ConfigParser()
+#         self.config.read('/Users/taipm/Documents/GitHub/microai.club/config.ini')
+
+#     def get(self, section, key):
+#         return self.config.get(section, key)
+    
+# import os
+# from configparser import ConfigParser
+
+# class Config:
+#     def __init__(self):
+#         self.config = ConfigParser()
+#         #self.config.read(os.environ.get('CONFIG_PATH'))
+
+#     def get(self, section, key):
+#         return self.config.get(section, key)
